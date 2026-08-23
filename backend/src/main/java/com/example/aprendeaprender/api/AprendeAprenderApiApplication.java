bc8970949591d34490d05156ff5fdb8e06b8c492
Andres Torres <andresardilatorres2@gmail.com>
2026-08-23 17:00:11 -0500

    feat: agregar punto de entrada de la API

---
+++ backend/src/main/java/com/example/aprendeaprender/api/AprendeAprenderApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.aprendeaprender.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendeAprenderApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AprendeAprenderApiApplication.class, args);
+    }
+}
